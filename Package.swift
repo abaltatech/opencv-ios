1918b6d2c7647b687d7b30ee6059393dfd06eb51
abalta-github-build <abalta-github-build@abaltatech.com>
2023-06-06 12:06:47 +0000

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let version = "4.7.0"
-let checksum = "ecb0ea13a349e6f02ec47a9e548b3904dc05cccc1610120f1b2f9d33eb730b55"
+let checksum = "4b7156c9a84b86ead98f45997efa0740cf3c47a78e110c472f366d31bac933ef"
 
 let package = Package(
     name: "OpenCV",
